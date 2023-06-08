0cbf81a1e142241452b64d6d8d39c1c90388a506
Alakh23 <121476873+Alakh23@users.noreply.github.com>
2023-06-09 00:37:45 +0530

    CURD Operation changes code

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import {NavigationContainer} from "@react-navigation/native"
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import Register from './screens/Register'
-import Home from './screens/Home'
-import Login from './screens/Login'
+import React, {useEffect, useState} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Register from './screens/Register';
+import Home from './screens/Home';
+import Login from './screens/Login';
+import CreateContact from './screens/CreateContact';
+import ContactDetails from './screens/ContactDetails';
 const App = () => {
   const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="login">
+      <Stack.Navigator initialRouteName={'login'}>
         <Stack.Screen
           name="login"
           component={Login}
@@ -25,10 +27,20 @@ const App = () => {
           component={Home}
           options={{headerShown: false}}
         />
+        <Stack.Screen
+          name="createContact"
+          component={CreateContact}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="contactDetails"
+          component={ContactDetails}
+          options={{headerShown: false}}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
 
 export default App
 
